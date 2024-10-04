be5d0eb5195ce4064e4f32ddb894767418c8b41a
FerPonce42 <angel.pacsi@ucsp.edu.pe>
2024-10-03 19:57:07 -0500

    todo list terminado

--- TODOLIST/app.py
+++ TODOLIST/app.py
@@ -13,18 +13,18 @@ def index():
 @app.route("/convert", methods=["POST"])
 def convert():
 
-    # Query for currency exchange rate
+    # Consulta para obtener la tasa de cambio de divisas
     currency = request.form.get("currency")
     res = requests.get(
         "http://data.fixer.io/api/latest",
         params={"access_key": "b42cc38fe4cd5b869888c5a9902aeb3c", "symbols": currency}
     )
 
-    # Make sure request succeeded
+    # Asegurarse de que la solicitud fue exitosa
     if res.status_code != 200:
         return jsonify({"success": False})
 
-    # Make sure currency is in response
+    # Asegurarse de que la moneda esté en la respuesta
     data = res.json()
     if currency not in data.get("rates", []):
         return jsonify({"success": False})
