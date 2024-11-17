6d92dbb87f09864b2e9fbfba226131102d9b86fc
FerPonce42 <angel.pacsi@ucsp.edu.pe>
2024-11-17 17:36:53 -0500

    eh vamos mejorando

--- TAREAS/XAMFROMCV/XamFormCV/XamFormCV/XamFormCV/obj/Debug/netstandard2.0/XamFormCV.AssemblyInfo.cs
+++ TAREAS/XAMFROMCV/XamFormCV/XamFormCV/XamFormCV/obj/Debug/netstandard2.0/XamFormCV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XamFormCV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e30e4f7aa4edb02dee5323de9648e08e7d9f92ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("XamFormCV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XamFormCV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
